4db1b098e6d3e4bc29ad8d0ca952ebacab572375
harabat <45575009+harabat@users.noreply.github.com>
2024-07-01 18:09:55 +0200

    add scraping script

---
+++ code/scraping.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from time import sleep
+import requests
+import json
+
+# fichier avec les codes des circonscriptions
+df = pd.read_json('./data/codes_circos.json')
+
+results = {}
+
+for i, code_circo in df['code'].items():
+    print(f'Downloading {code_circo} ({i + 1} out of 577)')
+
+    # itérer sur les urls en modifiant le code circo
+    url = f'https://apps.contexte.com/data/ctx-legislatives-2024/legislatives_2024/{code_circo}/results.json'
+    res = requests.get(url)
+
+    print(f'\tResults for {code_circo} downloaded\n\n')
+
+    # sauver le résultat dans le dict
+    result_circo = json.loads(res.content)
+    results[code_circo] = result_circo
+
+    # pour éviter le blocage
+    sleep(5)
+
+with open('./data/results.json', "w") as fp:
+    json.dump(results, fp=fp)
